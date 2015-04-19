f36c82a7245359a9714c9f4368d2f0d7c9f9563a
opussf <github@zz9-za.com>
2015-04-19 04:27:35 -0700

    Script to create a CSV of data

---
+++ src/GR_CSV.lua
@@ -0,0 +1,51 @@
+#!/usr/bin/env lua
+
+dataFile = "/Applications/World of Warcraft/WTF/Account/OPUSSF/SavedVariables/GoldRate.lua"
+
+function FileExists( name )
+   local f = io.open( name, "r" )
+   if f then io.close( f ) return true else return false end
+end
+function DoFile( filename )
+	local f = assert( loadfile( filename ) )
+	return f()
+end
+function PairsByKeys( t, f )
+	local a = {}
+	for n in pairs( t ) do table.insert( a, n ) end
+	table.sort( a, f )
+	local i = 0
+	local iter = function()
+		i = i + 1
+		if a[i] == nil then return nil
+		else return a[i], t[a[i]]
+		end
+	end
+	return iter
+end
+
+if FileExists( dataFile ) then
+	DoFile( dataFile )
+
+	print( "Realm,Faction,TimeStamp,Gold" )
+
+	for realm, rdata in pairs( GoldRate_data ) do
+		maxInitialTS = 0
+		for faction, fdata in pairs( rdata ) do
+			strOut = ""
+			for name, pdata in pairs( fdata.toons ) do
+				maxInitialTS = math.max( maxInitialTS, pdata.firstTS)
+			end
+			for ts, val in PairsByKeys( GoldRate_data.Hyjal.Alliance.consolidated ) do
+				if ts >= maxInitialTS then
+					strOut = strOut .. string.format( '%s,%s,%i,%i\n', realm, faction, ts, val )
+				end
+			end
+			print(strOut)
+		end
+	end
+
+
+
+
+end
